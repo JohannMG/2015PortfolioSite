a7b5044f490d1bc424facd010b6c652c973c81bd
JohannMG <hello@johannmg.com>
2015-08-30 18:21:32 -0400

    home page assemebled. still working on designs

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//menu button open
+document.getElementById('contact-btn').addEventListener('click', function (event) {
+	var button = document.getElementById('contact-btn'); 
+	var menu = document.getElementById('home-contact');
+
+	var classes = document.getElementById('contact-btn').classList;
+	var isActive = _.contains(classes, 'btn-active');
+
+	if (isActive){
+		classes.remove('btn-active');
+		menu.classList.remove('open');
+	}
+
+	else {
+		console.log(classes); 
+		classes.add('btn-active');
+		menu.classList.add('open'); 
+	}
+
+});
