500f63ef2cfda8b0da947c261f862aa9cee70fa9
JohannMG <johann1356@gmail.com>
2015-12-31 11:56:52 -0500

    About page is now responsive. Mobile menu shows on mobile with a CSS animation. Menu is opened and closed with classList.toggle() i've not had any browser issues with it yet Updated animations CSS. Nothing links out yet. There's no winter in Florida, save me.

--- js/page.js
+++ js/page.js
@@ -1,1 +1,16 @@
 //logic goes here
+
+//Add event listener for mobile menu button
+document.getElementById('mobilemenubutton').addEventListener('click', toggleMobileMenu);
+document.getElementById('lowerclosemenu').addEventListener('click', toggleMobileMenu);
+
+
+function toggleMobileMenu(){
+  var button   = document.getElementById('mobilemenubutton'); 
+  var menu     = document.getElementById('mobilemenu'); 
+
+  button.classList.toggle('active');
+  menu.classList.toggle('open');
+
+}
+
---
+++ serve.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /Users/johannmg/Dropbox/code/JohannMG-Site-2015/src 
+python -m SimpleHTTPServer 8080
+echo open
